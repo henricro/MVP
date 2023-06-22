7a13e31a87f511f26411eb4300124b6438ec93c4
Henri <henri.crozel@gmail.com>
2023-06-22 23:08:24 +0200

    startpage images again

--- MVP/static/js/images.js
+++ MVP/static/js/images.js
@@ -25,7 +25,7 @@ function createImage(note) {
 
     var name = XMLnote.getElementsByTagName("name")[0].childNodes[0].nodeValue;
 
-    if (["6220", "6219", "2288"].includes(id)) {
+    if ([6220, 6219, 2288].includes(id)) {
         var img_src = "/static/user_data/users/" + 1 + "/uploads/" + name;
     } else {
         var img_src = "/static/user_data/users/" + user_id + "/uploads/" + name;
