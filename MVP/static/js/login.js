1115b134332aa638fb62fbadb30646eb31d727a0
Henri <henri.crozel@gmail.com>
2023-07-26 17:21:17 +0100

    working on login page + shadows on most elements (looks nice :))

---
+++ MVP/static/js/login.js
@@ -0,0 +1,12 @@
+
+$("#updates_popup").hide();
+
+$("#updates_button").on('click', function() {
+    $("#updates_popup").toggle();
+});
+
+$("#patreon").on('click', function(){
+    window.open("https://www.patreon.com/gyst", '_blank');
+})
+
+
