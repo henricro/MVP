808ee41c303cbef227ccb9c9f171654cbe70f941
henri crozel <henri.crozel@gmail.com>
2023-08-18 00:15:16 +0200

    trying to download image from right click on it

--- MVP/views/download.py
+++ MVP/views/download.py
@@ -1,7 +1,7 @@
 from MVP import application, db, engine, user_required, basedir
 from MVP.models import *
 
-from flask import Flask, redirect, url_for, render_template, make_response, request, send_from_directory, send_file
+from flask import Flask, redirect, url_for, render_template, make_response, request, send_from_directory, send_file, Response
 from lxml import etree
 from pdf2image import convert_from_path
 
@@ -20,11 +20,16 @@ def download_image():
 
     request_data = request.get_json()
     relative_path = request_data.get('src')  # Assuming you send the path relative to '/static' in the JSON payload
+    filename = os.path.basename(relative_path)
 
     file_path = os.path.join(basedir, relative_path.lstrip('/'))
     print(file_path)
 
     if os.path.exists(file_path):
-        return send_file(file_path, as_attachment=True)
+        response = Response()
+        response.headers['Content-Disposition'] = f'attachment; filename="{filename}"'
+        response.headers['Content-Type'] = 'application/octet-stream'
+        response.headers['Cache-Control'] = 'no-cache'
+        return response
     else:
         return "File not found", 404
